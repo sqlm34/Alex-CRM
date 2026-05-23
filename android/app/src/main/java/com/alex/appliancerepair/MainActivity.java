1d8adc0666cdb704913db7c2bf2a15c80918b821
sqlm34 <28044725+sqlm34@users.noreply.github.com>
2026-05-23 11:51:11 -0400

    Add Android app wrapper

---
+++ android/app/src/main/java/com/alex/appliancerepair/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alex.appliancerepair;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alex.appliancerepair',
+  appName: 'Alex',
+  webDir: 'dist'
+};
+
+export default config;
