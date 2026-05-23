01573a04805904da4a74192779cb62de31032028
Александр Аксенов <sqlm38@gmail.com>
2026-05-23 14:19:08 -0400

    Deploy static build for Hostinger

--- android/app/src/main/java/com/alex/appliancerepair/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.alex.appliancerepair;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- backend/src/setup-db.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config'
-import { pool } from './db.js'
-import { createJobsTableSql } from './schema.js'
-
-await pool.query(createJobsTableSql)
-await pool.end()
-
-console.log('Database schema is ready')
